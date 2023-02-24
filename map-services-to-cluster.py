6491dffa1478a490445fb0260795eaee3f1ecbdd
Francis Huang <tan.huynh@setel.com>
2023-02-24 19:43:43 +0700

    feat: optimize algo

--- map-services-to-cluster.py
+++ map-services-to-cluster.py
@@ -18,17 +18,25 @@ ClusterServiceMap = dict[ClusterHostVal, dict['services', set[str]]]
 SecretDict = dict[ClusterHostNameKey, ClusterHostVal]
 OnlyHostSecretDict = SecretDict
 
+# Load dirs
+mongodb_secret_dirs: list[str] = []
+gitops_services_files: list[str] = []
+for root, dirs, files in os.walk(GITOPS_ROOT_DIR):
+    if re.search(f'{MONGODB_SHARED_SUB_DIR}(/.+)?/{environment}/aws/apse1', root):
+        mongodb_secret_dirs.append(root)
+    if not re.search(f'({MONGODB_SHARED_SUB_DIR}|\\.)', root) and files:
+        for file in files:
+            # ignore encrypted files
+            if not re.search(r'\.enc\.ya?ml', file):
+                gitops_services_files.append(os.path.join(root, file))
+
 
 def load_mongo_secret() -> SecretDict:
-    mongoshare_root_dir = os.path.join(GITOPS_ROOT_DIR, MONGODB_SHARED_SUB_DIR)
     secret_dict = {}
-    for root, dirs, files in os.walk(mongoshare_root_dir):
-        # Because the format inside mongodb-shared is not consistent, we will loop all subdirectories matching the env
-        for dir in dirs:
-            if dir == environment:
-                secret_path = os.path.join(root, dir, 'aws/apse1')
-                yaml_secret = subprocess.check_output(f'sops -d {secret_path}/secret.enc.yaml'.split(' '))
-                secret_dict = secret_dict | yaml.safe_load(yaml_secret)['stringData']
+    for dir in mongodb_secret_dirs:
+        yaml_secret = subprocess.check_output(f'sops -d {dir}/secret.enc.yaml'.split(' '))
+        secret_dict = secret_dict | yaml.safe_load(yaml_secret)['stringData']
+
     return secret_dict
 
 
@@ -47,26 +55,19 @@ def generate_name_host_dict(data: SecretDict) -> tuple[OnlyHostSecretDict, Clust
 
 def map_services_to_host(data: SecretDict) -> ClusterServiceMap:
     mongodb_hosts, cluster_list = generate_name_host_dict(data)
-    for root, *_ in os.walk(GITOPS_ROOT_DIR):
-        for _root, __, _files in os.walk(root):
-            # Only dive to dirs having files and not being mongodb-shared
-            if _files and not re.search(r'(mongodb-shared|\.)', _root):
-                for _file in _files:
-                    with open(os.path.join(_root, _file)) as f:
-                        try:
-                            content = f.read()
-                        except UnicodeDecodeError:
-                            print('error')
-                        hosts = set(re.findall(r'([A-Z0-9_]+_HOSTNAME_[A-Z0-9_]+)', content))
-                        app_name_label = re.search(r'gitops/([^/]+)/', _root)
-                        if hosts and app_name_label:
-                            app_name = app_name_label.groups()[0].strip()
-                            if app_name == 'mongodb-shared':
-                                print(app_name)
-                            for host in hosts:
-                                host_name = mongodb_hosts.get(host)
-                                if host_name and app_name:
-                                    cluster_list[host_name]['services'].add(app_name)
+    for gitops_file_path in gitops_services_files:
+        with open(gitops_file_path) as f:
+            try:
+                content = f.read()
+            except UnicodeDecodeError:
+                print('error')
+            hosts = set(re.findall(r'([A-Z0-9_]+_HOSTNAME_[A-Z0-9_]+)', content))
+            app_name_label = re.search(r'gitops/([^/]+)/', gitops_file_path)
+            if hosts and app_name_label:
+                app_name = app_name_label.groups()[0].strip()
+                for host_name in (mongodb_hosts.get(host) for host in hosts if app_name):
+                    cluster_list[host_name]['services'].add(app_name)
+
     for k, v in cluster_list.items():
         v['services'] = list(v['services'])
     return cluster_list
